f1cc15bef4c5dd0ac88105333a8349bd787ef259
edenlisk <nkumbuyenock@gmail.com>
2024-05-15 17:37:41 +0200

    Add helper methods and Ride metrics

---
+++ src/components/RideMetrics.jsx
@@ -0,0 +1,23 @@
+import PropTypes from "prop-types";
+
+const RideMetrics = ({durations, distance, nextStopAddress}) => {
+
+    return (
+        <div className='overflow-x-hidden w-[100%] z-50 bg-white text-gray-600 px-2 py-4 flex flex-col gap-1 justify-between items-start'>
+            <h2 className="text-4xl font-bold">Nyabugogo - Kimironko</h2>
+            <p className="text-3xl font-semibold">Next stop: {nextStopAddress}</p>
+            <p className="text-3xl font-semibold flex gap-2">
+                <span>Distance: {distance}</span>
+                <span>Time: {durations}</span>
+            </p>
+        </div>
+    )
+}
+
+RideMetrics.propTypes = {
+    durations: PropTypes.string,
+    distance: PropTypes.string,
+    nextStopAddress: PropTypes.string
+}
+
+export default RideMetrics;
